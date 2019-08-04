950c4271af67a475fd01be559109d8c7ee31773e
srikanthg <srikanthg@chromium.org>
2019-08-04 14:56:44 -0400

    Added some basic arguments to start with.

--- runner.py
+++ runner.py
@@ -4,18 +4,27 @@ The idea is to implement a framework that supports Mobile Automation
 using Appium.
 '''
 
+import argparse
 
-def parse_arguments(args):
-  pass
+def parse_arguments():
+  args_parser = argparse.ArgumentParser(description="Run Native Mobile Apps")
+  args_parser.add_argument('-p', '--platform',
+                           choices=['Android', 'iOS'],
+                           help='Choose platform to run against')
+  args_parser.add_argument('-d', '--device',
+                           choices=['Simulator', 'Real'],
+                           help='Choose platform to run against')
+  args_parser.add_argument('-host', '--appium-host', default='127.0.0.1',
+                           help='Host addresses to run appium port on.')
+  args_parser.add_argument('-port', '--appium-port', default=4723,
+                           help='Port to run appium server on')
+  args = args_parser.parse_args()
 
-def run(args=None):
+def main(passed_args=None):
   '''This will be the first function that gets called.
   The goal here is to read all the arguments that are passed to the script.
   '''
-  if args:
-    parse_arguments(args)
-  else:
-    print('You need to supply arguments.')
+  parse_arguments()
 
 if __name__ == '__main__':
-  run()
+  main()
