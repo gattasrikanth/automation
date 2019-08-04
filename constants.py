70337e84b3ac59e59bc76fb1a86fc6a494116033
srikanthg <srikanthg@chromium.org>
2019-08-04 15:17:12 -0400

    Create constants.py
    Start adding framework constants

---
+++ constants.py
@@ -0,0 +1,18 @@
+#  Copyright (c) 2019
+#  Author: Srikanth Gatta
+
+'''
+ALL (if not MOST) of the framework constants need tobe delclared in this file.
+This fill serves as main place to lookup all configurable constants.
+'''
+
+# Platforms
+ANDROID = 'Android'
+IOS = 'IOS'
+WEB = 'Desktop'
+
+APPIUM_DEFAULT_HOST = '127.0.0.1'
+APPIUM_DEFAULT_PORT = '4723'
+
+DEVICE_TYPE_REAL = 'Real'
+DEVICE_TYPE_SIM = 'Simulator'
