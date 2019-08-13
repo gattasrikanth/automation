dda610f823a6c1f70fda1e2c07dee18661c1422b
Srikanth <gattasrikanth@gmail.com>
2019-08-13 09:06:05 -0400

    Update appinfo.py

--- appinfo.py
+++ appinfo.py
@@ -1,5 +1,16 @@
 #  Copyright (c) 2019
 #  Author: Srikanth Gatta
 
+import constants
+import subprocess
+
 def GetIOSAppInfomation(app_path):
-  pass
+  '''
+  Get information about the specified iOS App.
+  Example: Name, Bundle Id, Version of the app.
+  :param app_path:
+  :return:
+  '''
+  bundle_id = subprocess.execute_process(
+                        subprocess.plist_buddy(app_path,
+                                               constants.IOS_BUNDLE_ID))
